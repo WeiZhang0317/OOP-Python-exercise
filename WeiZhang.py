926d3568347c4f490f36352d18abfc6c5cc65054
Xiangwei Zhang <Wei.Zhang2@lincolnuni.ac.nz>
2024-07-30 17:55:04 +1200

    add class Instructor and Participant

--- WeiZhang.py
+++ WeiZhang.py
@@ -10,8 +10,22 @@
 # the workshop. 
 # − A list of participants (objects of the Participant class) who attended the 
 # workshop.
+class Instructor:
+    def __init__(self, name, expertise, years_of_experience):
+        self.__name = name
+        self.__expertise = expertise
+        self.__years_of_experience = years_of_experience
+        self.__workshops = []
+        
+class Participant:
+    def __init__(self, name, registration_number, email):
+        self.__name = name
+        self.__registration_number = registration_number
+        self.__email = email
+        self.__enrolled_workshops = []
+        self.__waitlisted_workshops = []
 class Workshop:
-    def __init__(self,name,instructor, fee, maximum_capacity, date):
+    def __init__(self,name,instructor, date fee=1050, maximum_capacity=20):
         self.__workshop_name=name
         self.__workshop_instructor=instructor
         self.__workshop_fee= fee
@@ -20,6 +34,16 @@ class Workshop:
         self.__enrolled_participants=[]
         self.__waitlist_participants=[]
         self.__attended_participants=[]
+
+    def assign_instructor(self,instructor):
+        self.__workshop_instructor = instructor 
+    
+    def enrolled_participants(self,participants):
+        if len(__enrolled_participants)>= maximum_capacity:
+           __waitlist_participants.append(participants)    
+        else:
+            _enrolled_participants.append(participants) 
+               
 
 
 
