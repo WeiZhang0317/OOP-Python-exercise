10f47f34a7f537b709ba231a1146c19bbb48a502
Xiangwei Zhang <Wei.Zhang2@lincolnuni.ac.nz>
2024-10-15 18:14:26 +1300

    correct name

--- Assign 4/main.py
+++ Assign 4/main.py
@@ -2,10 +2,10 @@
 
 from db_config import engine, Base  
 from models.customer import Customer,CorporateCustomer
-from models.item import Item, Vegetable, WeightedVeggie, PackVeggie, UnitPriceVeggie, PremadeBox  
+from models.item import Item, Veggie, WeightedVeggie, PackVeggie, UnitPriceVeggie, PremadeBox  
 from models.order import Order, OrderLine
 from models.person import Person
-from models.order import Order
+from models.staff import Staff
 from models.payment import Payment, CreditCardPayment, DebitCardPayment
 
 
